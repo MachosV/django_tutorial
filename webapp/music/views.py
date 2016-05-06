bef2d37df2ffd2794bb6c2e9df737405cb69ff7e
sairous <razgriz9@gmail.com>
2016-05-06 17:41:13 +0300

    added templates added another view

--- webapp/music/views.py
+++ webapp/music/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Album
+
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("hello music section")
+    albums = Album.objects.all()
+    context = {
+        'albums': albums,
+    }
+    return render(request, 'music/index.html', context)
+
+
+def detail(request,album_id):
+    album = Album.objects.get(id=album_id)
+    context = {
+        'album': album
+    }
+    return render(request, 'music/details.html', context)
