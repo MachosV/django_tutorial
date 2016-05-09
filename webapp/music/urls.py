f5dd35ae31d657c54b0487fdbcc9e3f77e27075e
sairous <razgriz9@gmail.com>
2016-05-09 18:29:05 +0300

    details also return the songs of the album

--- webapp/music/urls.py
+++ webapp/music/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     #/music/
     url(r'^$',views.index,name="index"),
-    url(r'^$',views.index, name="index1"),
 
     #/music/#id
     url(r'^(?P<artist>.+)/(?P<album_title>.+)/$',views.details, name="details")
