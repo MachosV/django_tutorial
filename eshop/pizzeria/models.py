f93634eba18d7b411089d540e201df2450dbd7d9
sairous <razgriz9@gmail.com>
2016-05-24 17:36:48 +0300

    first commit of pizzeria app

---
+++ eshop/pizzeria/models.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+#class Product(models.Model):
+ #   name = models.CharField(max_length=30)
+  #  price = models.FloatField
+   # photo = models.CharField(max_length=200)
